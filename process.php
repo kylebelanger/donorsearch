d66616df44a6f62881ca438defd3f7814d45ebed
Kyle Belanger <belanger.kyle@gmail.com>
2016-06-29 18:10:12 -0400

    Processing text file

--- process.php
+++ process.php
@@ -6,11 +6,18 @@ define('DATABASE_USER', 'user');
 define('DATABASE_PASS', 'password');
 define('DATABASE_HOST', 'localhost');
 
+// Globals for data cache
+$city_array = [];
+$state_array = [];
+$country_array = [];
+
+
 // Include database class
 include_once('php/database.php');
 
 // Create new database object
 $DB = new DBPDO();
+
 
 // If uploaded file is valid
 if(fileValidation($_FILES["file"]) == true) {
@@ -65,14 +72,19 @@ function processFile($file) {
         }
     }
 
-    $arr = array();
+    /*for ($i = 0; $i < 10; $i++) {
+        $str = $organizations[$i];
 
-    for ($i = 0; $i < 10; $i++) {
-      # code...
-      $arr[] = $organizations[$i];
+        $arr = explode('|', $str);
+        print_r($arr);
+    }*/
+
+    for ($i = 0; $i < 50000; $i++) {
+        $str = $organizations[$i];
+
+        $arr = explode('|', $str);
+        insertRecord($arr);
     }
-
-    print_r($arr);
 
     // close the file
     fclose($file_contents);
@@ -87,6 +99,57 @@ function processFile($file) {
 */
 function insertRecord($record) {
 
+    global $city_array;
+    global $state_array;
+    global $country_array;
+
+    // check city for match
+    $city_key = checkMatch($city_array, $record[2]);
+    // check state for match
+    //$state_key = checkMatch($state_array, $record[3]);
+    // check country for match
+    //$country_key = checkMatch($country_array, $record[4]);
+
+    // SQL query
+    //$sql_insert = "INSERT "
+
+    //print_r($city_array);
+}
+
+/*  checkMatch
+ *  Check the global array variables for matching key values on this item
+ *  to avoid having duplicate data entries in table
+ *
+ *  @param Array (reference)
+ *  @param String - Value to check
+ *
+ *  @return Int - key value
+*/
+function checkMatch(&$type, $value) {
+
+      $match = false;
+      $return_key = null;
+
+      // for each item in cache
+      foreach ($type as $k => $v) {
+          if ($value === $v) {
+              $return_key = $k;
+              $match = true;
+          }
+      }
+
+      // if match is found
+      if ($match == true) {
+          // return key value for table reference
+          //print("Found match...");
+          return $return_key;
+      }
+      else {
+          // add to array
+          $type[] = $value;
+          // return new key value
+          return (count($type) - 1);
+      }
 }
 
 
