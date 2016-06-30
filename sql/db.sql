6fca70c59fe99f265564f865bfad4837288ee61d
Kyle Belanger <belanger.kyle@gmail.com>
2016-06-29 22:24:19 -0400

    Add index to database schema

--- sql/db.sql
+++ sql/db.sql
@@ -10,45 +10,24 @@ USE donorsearch;
 -- Begin creating tables
 -- ---------------------------
 
--- Create table: city (FK)
-CREATE TABLE city (
-	city_id				INT							PRIMARY KEY		AUTO_INCREMENT,
-	city_name			VARCHAR(150)		NOT NULL 			UNIQUE
-);
-
--- Create table: state (FK)
-CREATE TABLE state (
-	state_id			INT						PRIMARY KEY		AUTO_INCREMENT,
-	state_name		VARCHAR(2)		NOT NULL 			UNIQUE
-);
-
--- Create table: country (FK)
-CREATE TABLE country (
-	country_id				INT							PRIMARY KEY		AUTO_INCREMENT,
-	country_name			VARCHAR(150)		NOT NULL 			UNIQUE
-);
-
 -- Create table: organizations
 CREATE TABLE organization (
 	ein						INT							PRIMARY KEY		AUTO_INCREMENT,
 	legal_name		VARCHAR(150),
-	city_fk				INT,
-	state_fk			INT,
-	country_fk 		INT,
-	description		TEXT,
-
-	-- foreign key constraint
-	CONSTRAINT organization_fk_city
-		FOREIGN KEY (city_fk)
-		references city (city_id),
-
-	-- foreign key constraint
-	CONSTRAINT organization_fk_state
-		FOREIGN KEY (state_fk)
-		references state (state_id),
-
-		-- foreign key constraint
-		CONSTRAINT organization_fk_country
-			FOREIGN KEY (country_fk)
-			references country (country_id)
+	city_name			VARCHAR(150),
+	state_name		VARCHAR(150),
+	country_name 	VARCHAR(150),
+	description		TEXT
 );
+
+-- create index for city
+CREATE INDEX organization_city_name_ix
+	ON organization (city_name DESC);
+
+-- create index for state
+CREATE INDEX organization_state_name_ix
+	ON organization (state_name DESC);
+
+-- create index for country
+CREATE INDEX organization_country_name_ix
+	ON organization (country_name DESC);
